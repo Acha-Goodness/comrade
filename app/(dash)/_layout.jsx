a6c0cd78d7bfb7a61f307a96d43b408997b9861e
Acha-Goodness <acharious1668@gmail.com>
2026-07-08 20:12:37 +0100

    designed product details buttom sheet for market screen

--- app/(dash)/_layout.jsx
+++ app/(dash)/_layout.jsx
@@ -2,11 +2,13 @@ import { MaterialCommunityIcons, Octicons, FontAwesome, MaterialIcons } from "@e
 import { Tabs } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Platform, View, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const DashLayout = () => {
     return (
         <>
-            {/* {Platform.OS === "ios" ? (
+            <GestureHandlerRootView>
+                {/* {Platform.OS === "ios" ? (
                 <View style={{ height: "5%", backgroundColor: "#ffffff" }}>
                     <StatusBar style="dark" />
                 </View>
@@ -15,214 +17,214 @@ const DashLayout = () => {
                     <StatusBar style="dark" />
                 </View>
             )} */}
-            <StatusBar style="dark" />
-            <Tabs
-                screenOptions={{
-                    headerShown: false,
-                    tabBarShowLabel: false,
-                    tabBarStyle: {
-                        paddingTop: "5%",
-                        paddingHorizontal: "5%",
-                        borderRadius: 50,
-                        backgroundColor: "#ffffff",
-                        position: "absolute",
-                        bottom: 30,
-                        width: "90%",
-                        marginHorizontal: "5%",
-                        // iOS shadow
-                        shadowColor: "#000",
-                        shadowOffset: { width: 0, height: 8 },
-                        shadowOpacity: 0.15,
-                        shadowRadius: 12,
+                <StatusBar style="dark" />
+                <Tabs
+                    screenOptions={{
+                        headerShown: false,
+                        tabBarShowLabel: false,
+                        tabBarStyle: {
+                            paddingTop: "5%",
+                            paddingHorizontal: "5%",
+                            borderRadius: 50,
+                            backgroundColor: "#ffffff",
+                            position: "absolute",
+                            bottom: 30,
+                            width: "90%",
+                            marginHorizontal: "5%",
+                            // iOS shadow
+                            shadowColor: "#000",
+                            shadowOffset: { width: 0, height: 8 },
+                            shadowOpacity: 0.15,
+                            shadowRadius: 12,
 
-                        // Android shadow
-                        elevation: 10,
-                    },
-                }}
-            >
-                <Tabs.Screen
-                    name="dashboard"
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View
-                                className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
+                            // Android shadow
+                            elevation: 10,
+                        },
+                    }}
+                >
+                    <Tabs.Screen
+                        name="dashboard"
+                        options={{
+                            tabBarIcon: ({ focused }) => (
+                                <View
+                                    className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
                                 ${focused ? "bg-[#000000]" : "bg-transparent"}`}
-                            >
-                                <Octicons
-                                    name="home-fill"
-                                    size={focused ? 18 : 24}
-                                    color={focused ? "#FFFFFF" : "#000000"}
-                                />
+                                >
+                                    <Octicons
+                                        name="home-fill"
+                                        size={focused ? 18 : 24}
+                                        color={focused ? "#FFFFFF" : "#000000"}
+                                    />
 
-                                {focused && (
-                                    <Text className="text-white font-semibold text-sm ml-2">
-                                        Home
-                                    </Text>
-                                )}
-                            </View>
-                        ),
-                    }}
-                />
+                                    {focused && (
+                                        <Text className="text-white font-semibold text-sm ml-2">
+                                            Home
+                                        </Text>
+                                    )}
+                                </View>
+                            ),
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="campus"
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View
-                                className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
+                    <Tabs.Screen
+                        name="campus"
+                        options={{
+                            tabBarIcon: ({ focused }) => (
+                                <View
+                                    className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
                                 ${focused ? "bg-[#000000]" : "bg-transparent"}`}
-                            >
-                                <FontAwesome
-                                    name="building"
-                                    size={focused ? 18 : 24}
-                                    color={focused ? "#FFFFFF" : "#000000"}
-                                />
-                                {focused && (
-                                    <Text className="text-white font-semibold text-sm ml-2">
-                                        Campus
-                                    </Text>
-                                )}
-                            </View>
-                        ),
-                    }}
-                />
+                                >
+                                    <FontAwesome
+                                        name="building"
+                                        size={focused ? 18 : 24}
+                                        color={focused ? "#FFFFFF" : "#000000"}
+                                    />
+                                    {focused && (
+                                        <Text className="text-white font-semibold text-sm ml-2">
+                                            Campus
+                                        </Text>
+                                    )}
+                                </View>
+                            ),
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="market"
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View
-                                className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
+                    <Tabs.Screen
+                        name="market"
+                        options={{
+                            tabBarIcon: ({ focused }) => (
+                                <View
+                                    className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
                                 ${focused ? "bg-[#000000]" : "bg-transparent"}`}
-                            >
-                                <MaterialIcons
-                                    name="shopping-cart"
-                                    size={focused ? 18 : 24}
-                                    color={focused ? "#FFFFFF" : "#000000"}
-                                />
+                                >
+                                    <MaterialIcons
+                                        name="shopping-cart"
+                                        size={focused ? 18 : 24}
+                                        color={focused ? "#FFFFFF" : "#000000"}
+                                    />
 
-                                {focused && (
-                                    <Text className="text-white font-semibold text-sm ml-2">
-                                        Market
-                                    </Text>
-                                )}
-                            </View>
-                        ),
-                    }}
-                />
+                                    {focused && (
+                                        <Text className="text-white font-semibold text-sm ml-2">
+                                            Market
+                                        </Text>
+                                    )}
+                                </View>
+                            ),
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="message"
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View
-                                className={`flex-row items-center w-[88px] justify-center h-10 px-4 rounded-[10px]
+                    <Tabs.Screen
+                        name="message"
+                        options={{
+                            tabBarIcon: ({ focused }) => (
+                                <View
+                                    className={`flex-row items-center w-[88px] justify-center h-10 px-4 rounded-[10px]
                                 ${focused ? "bg-[#000000]" : "bg-transparent"}`}
-                            >
-                                <MaterialCommunityIcons
-                                    name="message-text-fast"
-                                    size={focused ? 18 : 24}
-                                    color={focused ? "#FFFFFF" : "#000000"}
-                                />
+                                >
+                                    <MaterialCommunityIcons
+                                        name="message-text-fast"
+                                        size={focused ? 18 : 24}
+                                        color={focused ? "#FFFFFF" : "#000000"}
+                                    />
 
-                                {focused && (
-                                    <Text className="text-white font-semibold text-sm ml-2">
-                                        Message
-                                    </Text>
-                                )}
-                            </View>
-                        ),
-                    }}
-                />
+                                    {focused && (
+                                        <Text className="text-white font-semibold text-sm ml-2">
+                                            Message
+                                        </Text>
+                                    )}
+                                </View>
+                            ),
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="profile"
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View
-                                className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
+                    <Tabs.Screen
+                        name="profile"
+                        options={{
+                            tabBarIcon: ({ focused }) => (
+                                <View
+                                    className={`flex-row items-center w-[80px] justify-center h-10 px-4 rounded-[10px]
                                 ${focused ? "bg-[#000000]" : "bg-transparent"}`}
-                            >
-                                <MaterialCommunityIcons
-                                    name="account-school"
-                                    size={focused ? 18 : 24}
-                                    color={focused ? "#FFFFFF" : "#000000"}
-                                />
+                                >
+                                    <MaterialCommunityIcons
+                                        name="account-school"
+                                        size={focused ? 18 : 24}
+                                        color={focused ? "#FFFFFF" : "#000000"}
+                                    />
 
-                                {focused && (
-                                    <Text className="text-white font-semibold text-sm ml-2">
-                                        Profile
-                                    </Text>
-                                )}
-                            </View>
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name="createThrift"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                                    {focused && (
+                                        <Text className="text-white font-semibold text-sm ml-2">
+                                            Profile
+                                        </Text>
+                                    )}
+                                </View>
+                            ),
+                        }}
+                    />
+                    <Tabs.Screen
+                        name="createThrift"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="createAjo"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                    <Tabs.Screen
+                        name="createAjo"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="statistics"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                    <Tabs.Screen
+                        name="statistics"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="kycLevel"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                    <Tabs.Screen
+                        name="kycLevel"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="kycVerification"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                    <Tabs.Screen
+                        name="kycVerification"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="ninVerification"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                    <Tabs.Screen
+                        name="ninVerification"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="addressVerification"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
+                    <Tabs.Screen
+                        name="addressVerification"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
 
-                <Tabs.Screen
-                    name="kycSuccess"
-                    options={{
-                        href: null,
-                        tabBarStyle: { display: 'none' }
-                    }}
-                />
-
-            </Tabs>
+                    <Tabs.Screen
+                        name="kycSuccess"
+                        options={{
+                            href: null,
+                            tabBarStyle: { display: 'none' }
+                        }}
+                    />
+                </Tabs>
+            </GestureHandlerRootView>
         </>
     )
 }
--- app/(dash)/market.jsx
+++ app/(dash)/market.jsx
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, ScrollView, TextInput, Dimensions, FlatList, Image } from 'react-native';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { EvilIcons, Feather, Ionicons } from '@expo/vector-icons';
 import { router, useLocalSearchParams } from 'expo-router';
@@ -14,23 +14,36 @@ import ebike from "@/assets/images/ebike.png";
 import devices from "@/assets/images/elec.png";
 import headset from "@/assets/images/headset.png";
 import bob from "@/assets/images/bob.png";
+import propic from "@/assets/images/propic.jpg";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 const { width } = Dimensions.get('window');
 
 const Market = () => {
-    const [isPresented, setIsPresented] = useState(false);
     const params = useLocalSearchParams();
+    const bottomSheetRef = useRef(null);
+    const snapPoints = useMemo(() => [0.1, '66%'], [])
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
     const product = [
-        { id: 1, text: 'Electronic Bike', price: "N10,000", distance: "500m away", img: ebike },
-        { id: 2, text: 'Wireless Headphones', price: "N10,000", distance: "500m away", img: headset },
-        { id: 3, text: 'Ergonomic Chair', price: "N10,000", distance: "500m away", img: seat },
-        { id: 4, text: 'Device', price: "N10,000", distance: "500m away", img: devices },
+        { id: 1, text: 'Electronic Bike', price: "N10,000", distance: "500m away", img: ebike, desc: "High quality sound with noise cancellation, used only for two month" },
+        { id: 2, text: 'Wireless Headphones', price: "N10,000", distance: "500m away", img: headset, desc: "High quality sound with noise cancellation, used only for two month" },
+        { id: 3, text: 'Ergonomic Chair', price: "N10,000", distance: "500m away", img: seat, desc: "High quality sound with noise cancellation, used only for two month" },
+        { id: 4, text: 'Device', price: "N10,000", distance: "500m away", img: devices, desc: "High quality sound with noise cancellation, used only for two month" },
     ]
 
+    const handleClosePress = () => {
+        bottomSheetRef.current?.close();
+    }
+
+    const handleOpenPress = (item) => {
+        setSelectedProduct(item)
+        bottomSheetRef.current?.expand();
+    }
+
     return (
-        <SafeAreaView className="flex-1 bg-[#FFFFFF] p-[20%]">
+        <SafeAreaView className="flex-1 bg-[#ffffff] p-[20%]">
             <View className="flex-1">
                 <View className="flex-row items-center justify-between">
                     <View></View>
@@ -48,13 +61,13 @@ const Market = () => {
                 <View className="mt-[3%] flex-row items-center justify-center gap-2">
                     <TextInput
                         placeholder="Search"
-                        className="w-[80%] py-4 px-3 rounded-[10px] border border-2 border-[#F0F0F0]"
+                        className="w-[80%] py-4 px-3 rounded-[10px] bg-[#ffffff] border border-2 border-[#F0F0F0]"
                         id="search"
                         type="text"
                         autoCapitalize="none"
                         placeholderTextColor="#777"
                     />
-                    <TouchableOpacity className="p-3 rounded-[10px] border border-2 border-[#F0F0F0]">
+                    <TouchableOpacity className="p-3 rounded-[10px] bg-[#ffffff] border border-2 border-[#F0F0F0]">
                         <Feather name="sliders" size={25} color="#000000ff" />
                     </TouchableOpacity>
                 </View>
@@ -99,21 +112,23 @@ const Market = () => {
                         numColumns={2}
                         showsHorizontalScrollIndicator={false}
                         columnWrapperStyle={{
-                            justifyContent: "space-evenly",
+                            justifyContent: "space-between",
                             marginBottom: 10,
                         }}
-                        keyExtractor={item => item.id.toString()}
+                        keyExtractor={item => item.id}
                         renderItem={({ item }) => (
-                            <TouchableOpacity className={`w-[45%] rounded-[5px]`}>
+                            <TouchableOpacity onPress={() => handleOpenPress(item)} className={`w-[45%] rounded-[5px]`}>
                                 <View className='bg-[#e0e0e0ff] w-full h-[13vh] rounded-t-md items-center justify-center'>
                                     <View className='px-[2.5%] py-[2.5%] mt-1 rounded-[5px] bg-[#0452df] absolute top-1 left-2.5 z-10'>
                                         <Ionicons name="shield-checkmark" size={15} color="#ffffffff" />
                                     </View>
-                                    <Image
-                                        source={item.img}
-                                        className="w-[60%]"
-                                        resizeMode="contain"
-                                    />
+                                    <View className='w-[50%] h-[15vh]'>
+                                        <Image
+                                            source={item.img}
+                                            className="w-[100%] h-[100%]"
+                                            resizeMode="contain"
+                                        />
+                                    </View>
                                 </View>
                                 <View className='px-2'>
                                     <Text className="text-[12px] text-[#000] font-poppins-medium mt-2">{item.text}</Text>
@@ -144,10 +159,63 @@ const Market = () => {
                         />
                     </View>
                 </View>
-                <BottomSheetView>
-                    <Text>SHEEEEEEET</Text>
-                </BottomSheetView>
             </View>
+
+            <BottomSheet ref={bottomSheetRef} index={0} snapPoints={snapPoints}>
+                <BottomSheetView className='p-2'>
+                    <View className='bg-[#e0e0e0ff] w-full h-[20vh] rounded-t-md items-center justify-center'>
+                        <View className='px-[2.5%] py-[2.5%] mt-1 rounded-[5px] bg-[#0452df] absolute top-1 left-2.5 z-10'>
+                            <Ionicons name="shield-checkmark" size={15} color="#ffffffff" />
+                        </View>
+                        <View className='w-[80%] h-[20vh]'>
+                            <Image
+                                source={selectedProduct?.img}
+                                className="w-[100%] h-[100%]"
+                                resizeMode="contain"
+                            />
+                        </View>
+                    </View>
+                    <View className='px-3'>
+                        <Text className="text-[14px] text-[#000] font-poppins-medium mt-2">{selectedProduct?.text}</Text>
+                        <View className='flex-row justify-between items-center'>
+                            <Text className="text-[12px] text-[#000] font-poppins-medium mt-1">{selectedProduct?.price}</Text>
+                            <Ionicons name="heart-outline" size={15} color="#000000" />
+                        </View>
+                        <Text className="w-[60%] text-[10px] text-[#878787ff] font-poppins-medium mt-1">{selectedProduct?.desc}</Text>
+                    </View>
+                    <View className='mt-2 px-3'>
+                        <View className='flex-row justify-between items-start px-2'>
+                            <View className='flex-row items-center h-[10vh]'>
+                                <View className='w-[90px] h-[90px] flex justify-center'>
+                                    <Image
+                                        source={propic}
+                                        className="w-[70%] h-[70%] rounded-[50px]"
+                                        resizeMode="objeect-fit"
+                                    />
+                                </View>
+                                <View className='relative left-[-8px]'>
+                                    <Text className='text-[14px]'>Amanda Brook</Text>
+                                    <Text className='text-[12px] mt-2'>CS * 3rd Year</Text>
+                                    <Text className='text-[10px]'>⭐️ 4.8</Text>
+                                </View>
+                            </View>
+                            <Ionicons name="shield-checkmark" size={15} color="#0452df" />
+                        </View>
+                        <View className='flex-row gap-2 mt-5'>
+                            <Ionicons name="shield-checkmark" size={15} color="#0452df" />
+                            <Text className='text-[12px]'>2.4km away from you</Text>
+                        </View>
+                        <View className='flex-row gap-2 mt-2'>
+                            <TouchableOpacity className="bg-white py-[8px] rounded-[5px] mt-2 w-[30%] border border-1 border-[#0452df]">
+                                <Text className="text-[12px] font-[500] text-center font-inter text-[#0452df]">Chat</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity className="bg-[#0452df] py-[8px] rounded-[5px] mt-2 w-[30%]">
+                                <Text className="text-[12px] font-[500] text-center font-inter text-[#ffffff]">Make Offer</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </BottomSheetView>
+            </BottomSheet>
         </SafeAreaView>
     )
 }
